8bdafd86a89313ef260e124b92663defd12af86d
钟阳山 <1964005690@qq.com>
2021-11-10 18:35:33 +0800

    2021/11/10 周三 18:35:33.31

---
+++ typeScript/self02/index.ts
@@ -0,0 +1,17 @@
+class hello {
+  name1
+  constructor(name: any) {
+    this.name1 = name
+  }
+  get myName() {
+    return this.name1
+  }
+  set myName(value) {
+    this.name1 = value
+  }
+}
+
+let a = new hello(222)
+console.log(a.myName);
+a.myName = 8881
+console.log(a.name1);
