4bd124e60e3300083db6aae557ecff8cc512c3c1
钟阳山 <1964005690@qq.com>
2021-11-12 18:35:33 +0800

    2021/11/12 周五 18:35:33.12

--- typeScript/self02/index.js
+++ typeScript/self02/index.js
@@ -1,4 +1,24 @@
-function identity(arg) {
-    return arg;
+// class hello {
+//   name1
+//   constructor(name: any) {
+//     this.name1 = name
+//   }
+//   get myName() {
+//     return this.name1
+//   }
+//   set myName(value) {
+//     this.name1 = value
+//   }
+// }
+// let a = new hello(222)
+// console.log(a.myName);
+// a.myName = 8881
+// console.log(a.name1);
+
+function thisUse(){
+    this.name = '钟阳山'
+    console.log(this);
 }
-console.log(identity);
+
+let use = new thisUse()
+console.log(use.name);
--- typeScript/self02/index.ts
+++ typeScript/self02/index.ts
@@ -1,17 +1,23 @@
-class hello {
-  name1
-  constructor(name: any) {
-    this.name1 = name
-  }
-  get myName() {
-    return this.name1
-  }
-  set myName(value) {
-    this.name1 = value
-  }
+// class hello {
+//   name1
+//   constructor(name: any) {
+//     this.name1 = name
+//   }
+//   get myName() {
+//     return this.name1
+//   }
+//   set myName(value) {
+//     this.name1 = value
+//   }
+// }
+// let a = new hello(222)
+// console.log(a.myName);
+// a.myName = 8881
+// console.log(a.name1);
+
+function thisUse(){
+  this.name = '钟阳山'
 }
-te
-let a = new hello(222)
-console.log(a.myName);
-a.myName = 8881
-console.log(a.name1);
+
+let use = new thisUse()
+console.log(use.name);
