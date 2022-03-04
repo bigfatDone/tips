a0e97cd53a7951f6a0fa3fa696a501192e644011
钟阳山 <1964005690@qq.com>
2022-03-04 19:35:52 +0800

    2022/03/04 周五 19:35:52.19

--- 掘进view之道联系题/14service-worker/ws.js
+++ 掘进view之道联系题/14service-worker/ws.js
@@ -2,17 +2,18 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('my-cache').then(function(cache) {
-      return cache.addAll(['./index.html'])
+      return cache.addAll(['./index.html', './ws.js'])
     })
   )
 })
 
-// 拦截所有请求事件
+// 拦截所有请求事件,这个拦截请求是拦截请求文件的请求，例如index.html, ws.js
 // 如果缓存中已经有请求的数据就直接用缓存，否则去请求数据
 self.addEventListener('fetch', e => {
+  // debugger
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      if (response) {
+      if (response) { // 有缓存， 返回缓存
         return response
       }
       console.log('fetch source')
